96c8b3ccb8c46d9b3a02f53de7490bc087b7cf37
unknown <you@example.comadriclass@hotmail.comgit config --global user.name AdriCuello>
2022-07-04 21:23:32 -0300

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,18 @@
+var boton=document.querySelector(".header_icono")
+var menu=document.querySelector(".header__nav")
+var cont=0
+
+boton.onclick=function(){
+  if(cont%2==0){
+    menu.classList.add("header__nav--active")
+    menu.classList.remove("header__nav--disabled")
+  }
+
+  else{
+    menu.classList.remove("header__nav--active")
+    menu.classList.add("header__nav--disabled")
+  }
+
+  cont++
+}
+// Audit usage of navigator.userAgent, navigator.appVersion, and navigator.platform
